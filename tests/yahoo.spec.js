6be4d97c739d8975b99867e3fafb4d261fb90ff6
chris <chrisxmonahan@gmail.com>
2023-09-05 20:31:28 -0400

    proper done of playwright

--- tests/yahoo.spec.js
+++ tests/yahoo.spec.js
@@ -1,10 +1,18 @@
+//import { test, expect } from '@playwright/test';
+
+//test('yahoo search checker', async ({ page }) => {
+// await page.goto('https://www.yahoo.com/');
+//  await page.getByLabel('Search', { exact: true }).click();
+//  await page.getByLabel('Search', { exact: true }).fill('timberframe');
+//  await page.getByLabel('Yahoo Search').first().click();
+//  await page.getByRole('link', { name: 'Images', exact: true }).click();
+//  await page.waitForTimeout(3000);
+//});
+
 import { test, expect } from '@playwright/test';
 
-test('yahoo search checker', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('https://www.yahoo.com/');
-//  await page.getByLabel('Search', { exact: true }).click();
-  await page.getByLabel('Search', { exact: true }).fill('timberframe');
-  await page.getByLabel('Yahoo Search').first().click();
-  await page.getByRole('link', { name: 'Images', exact: true }).click();
-  await page.waitForTimeout(3000);
+  await page.goto('https://search.yahoo.com/search?p=timberframe&fr=yfp-t&fr2=p%3Afp%2Cm%3Asb&ei=UTF-8&fp=1');
+  await page.getByRole('tab', { name: 'Images' }).getByRole('link').click();
 });
